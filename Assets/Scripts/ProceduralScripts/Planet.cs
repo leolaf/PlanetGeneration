d78fb046e89bcf958b07f69900b04127ea0b07e5
Léo Laffont <leo.laffont@etu.univ-smb.fr>
2023-12-12 08:34:45 +0100

    Multiple noise filter types and planet face mask for greater rendering

--- Assets/Scripts/ProceduralScripts/Planet.cs
+++ Assets/Scripts/ProceduralScripts/Planet.cs
@@ -7,6 +7,8 @@ public class Planet : MonoBehaviour
     [Range(2, 256)]
     public int resolution = 10;
     public bool autoUpdate = true;
+    public enum FaceRenderMask { All, Top, Bottom, Left, Right, Front, Back };
+    public FaceRenderMask faceRenderMask;
 
     public ShapeSettings shapeSettings;
     public ColorSettings colorSettings;
@@ -51,6 +53,8 @@ public class Planet : MonoBehaviour
             }
 
             terrainFaces[i] = new TerrainFace(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
+            bool renderFace = faceRenderMask == FaceRenderMask.All || (int)faceRenderMask - 1 == i;
+            meshFilters[i].gameObject.SetActive(renderFace);
         }
     }
 
@@ -77,9 +81,12 @@ public class Planet : MonoBehaviour
 
     void GenerateMesh()
     {
-        foreach(TerrainFace face in terrainFaces)
+        for(int i = 0; i < meshFilters.Length; i++)
         {
-            face.ConstructMesh();
+            if (meshFilters[i].gameObject.activeSelf)
+            {
+                terrainFaces[i].ConstructMesh();
+            }
         }
     }
 
